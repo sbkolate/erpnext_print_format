f750086dd887ca4b5637833d0f5db55b3ab466e1
Amirmansi <163479758+Amirmansi@users.noreply.github.com>
2024-11-03 17:00:44 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='erpnext_print_format',
+    version='0.0.1',
+    description='ERPNext custom print formats',
+    packages=['erpnext_print_format'],
+    include_package_data=True
+)
